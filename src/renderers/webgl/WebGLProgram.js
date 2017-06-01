4c8ff0382d4864d46b294b156960e97d964edc26
Jay Hsia <jay@hover.to>
2017-06-01 14:55:44 -0700

    Finally add that forgotten var….

--- src/renderers/webgl/WebGLProgram.js
+++ src/renderers/webgl/WebGLProgram.js
@@ -197,7 +197,7 @@ function unrollLoops( string ) {
 function djb2Code(str){
     var hash = 5381;
     for (var i = 0; i < str.length; i++) {
-        char = str.charCodeAt(i);
+        var char = str.charCodeAt(i);
         hash = ((hash << 5) + hash) + char; /* hash * 33 + c */
     }
     return hash;
